6084ef75b5cb69ecc8e6d7d8cb07efed37af72c2
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-10-07 13:28:00 +0530

    inventory list: useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import InventoryList from "./components/InventoryList"
 
 
 function App() {
@@ -5,8 +6,9 @@ function App() {
   return (
     <>
       <h1
-        className="text-3xl text-center text-red-700"
-      >Welcome to Vite with TailwindCSS and React</h1>
+        className="text-3xl text-center text-red-500"
+      >Welcome to Vite with TailwindCSS and React </h1>
+      <InventoryList/>
     </>
   )
 }
